a1cc34b7db3f557cd8f0e12aade1bc1d7472403b
hyeonjin <gdalove@naver.com>
2023-02-06 23:57:08 +0900

    인증정보 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Nav from "./components/nav/Nav";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 const App = () => {
+  const { isAuthReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Login" element={<Login />}></Route>
-          <Route path="/Signup" element={<Signup />}></Route>
-        </Routes>
-      </BrowserRouter>
+      {isAuthReady ? (
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                user ? (
+                  <Home />
+                ) : (
+                  <Navigate replace={true} to="/login"></Navigate>
+                )
+              }
+            ></Route>
+
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" replace={true} />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" replace={true} />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        "loading..."
+      )}
     </div>
   );
 };
