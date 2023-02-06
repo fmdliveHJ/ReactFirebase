9b963e8f2d91c384d463903926618a468c6f5bd5
hyeonjin <gdalove@naver.com>
2023-02-06 23:57:25 +0900

    인증정보 추가 수정

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,6 @@
-import { createContext, useReducer } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useReducer } from "react";
+import { appAuth } from "../firebase/config";
 
 //컨텍스트 객체를 담아줌
 const AuthContext = createContext();
@@ -10,6 +12,8 @@ const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "logout":
       return { ...state, user: null };
+    case "isAuthReady":
+      return { ...state, user: action.payload, isAuthReady: true };
     default:
       return state;
   }
@@ -22,7 +26,18 @@ const AuthContextProvider = ({ children }) => {
   //state를 업데이트 할 수 있는 함수 dispatch
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
+    isAuthReady: false,
   });
+
+  useEffect(() => {
+    // onAuthStateChanged : 유저의 인증정보 변화를 관찰하는 함수입니다.
+    //onAuthStateChanged 함수는 Unsubscribe 함수를 반환합니다. 더 이상 유저의 변화를 관찰하지 않도록 하는 함수입니다.
+    //우리는 새로고침 후 초기에 딱 한번 실행하면 되기 때문에 이후에는 구독을 중지합니다.
+    const unsubscribe = onAuthStateChanged(appAuth, (user) => {
+      dispatch({ type: "isAuthReady", payload: user });
+      unsubscribe();
+    });
+  }, []);
   console.log("state", state);
   //dispatch > authReducer호출 > state 업데이트
   //dispatch 함수를 context에 담에서 전역에서 공유
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -5,20 +5,23 @@ import { useAuthContext } from "./useAuthContext";
 export const useLogout = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useAuthContext;
+
+  const { dispatch } = useAuthContext();
   const logout = () => {
-    setError(null);
-    setIsPending(true);
+    setError(null); // 아직 에러가 없으니 null 입니다.
+    setIsPending(true); // 통신중이므로 true입니다.
 
     signOut(appAuth)
       .then(() => {
+        // 로그아웃 성공!
         dispatch({ type: "logout" });
         setError(null);
         setIsPending(false);
       })
-      .catch((error) => {
-        setError(error.message);
+      .catch((err) => {
+        setError(err.message);
         setIsPending(false);
+        console.log(err.message);
       });
   };
   return { error, isPending, logout };
